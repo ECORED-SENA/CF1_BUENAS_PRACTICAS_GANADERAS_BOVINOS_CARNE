dd07325a4d087f71b257fd3d1a90098444913a75
SANDRATRIANA <sandrajem23@hotmail.com>
2022-05-17 14:45:34 -0500

    avance 7

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -4,11 +4,18 @@
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
     p.mb-5 En este punto del componente se invita a visualizar con atención el esquema que aquí se presenta. Analice cada elemento y concepto mostrados en este y realice su propia síntesis del contenido visto. ¡Adelante!
-    .row.justify-content-center
+    .row.mb-5.justify-content-center
       .col-lg-11
         figure
           img(src="@/assets/curso/sintesis.svg", alt="alt")
-
+    .row.justify-content-center 
+      .col-auto
+        a.anexo.mb-4(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
+          .anexo__icono
+            img(src="@/assets/template/icono-pdf.svg")
+          .anexo__texto
+            p Anexo. Síntesis
+          
 </template>
 
 <script>
